e9dcac997ca228193bb83722ecd5be12e35f55a7
Ivan Stroganov <knowhere@gmail.com>
2012-11-02 23:21:26 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaiverSigner::Application.config.secret_token = 'ea4471ca077ba2828e28c77424b8f8608d0c65d8633314839b28e2626b346803568aaf0620b66c9229c2dceb59344280563ed8ddef1243673943d4aa2d51f0f3'
